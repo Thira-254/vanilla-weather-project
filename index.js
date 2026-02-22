d658b9f3b41019a007f59dc3eed59811ae716da5
Thira-254 <agathathira60@gmail.com>
2026-02-22 11:40:56 +0300

    updated HTML and added javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+const apiKey = " 4do4bca6e6423fa301eeabet0fd1ad71";
+const apiUrl =
+  " https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+const form = document.querySelector("form");
+const cityInput = document.querySelector(".city");
